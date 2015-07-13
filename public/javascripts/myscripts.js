33bd1556cc24c1062ede89f97db17d0fa8a8b6eb
gianlucazigni <gianlucazigni@gmail.com>
2015-07-13 14:38:00 -0400

    finalized everything

---
+++ public/javascripts/myscripts.js
@@ -0,0 +1,6 @@
+	$(document).ready(function() {
+      $('select').material_select();
+  	});
+  	$(".dropdown-button").dropdown();
+  	$('.fixed-action-btn').openFAB();
+  	$('.fixed-action-btn').closeFAB();
