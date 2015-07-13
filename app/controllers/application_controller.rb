7a735e4aa12a5fb30775b5b33a3eafed00273bc6
c650 <c650@users.noreply.github.com>
2015-07-13 13:16:49 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ require "sinatra/base"
 
 class ApplicationController < Sinatra::Base
 
+<<<<<<< HEAD
+# register Sinatra::Flash
+=======
+>>>>>>> 163ffaf171747be9499daea58e1ec46608872ce4
 
 	configure do
 		set :public_folder, 'public'
@@ -13,16 +17,54 @@ class ApplicationController < Sinatra::Base
 		set :sessions, true
 		set :session_secret, "/plz_dnt_hrt_mh"
 	end
-
+######ROOT PAGE METHODS######
 	get '/' do
 		@users = User.all
 		@posts = Post.all
 		erb :index
 	end
 
-	post '/' do
-		
+	post '/' do	
 		@posts = Post.all
 	end
+######LOGIN METHODS######
+	get '/login' do
+        erb :login
+  	end
 
+  	post '/login' do
+	    @user = User.find_by(username: params[:username])
+	    if @user
+	      session[:user_id] = @user.id
+	      redirect to '/'
+	    else
+	      redirect to '/login'
+	    end
+  	end
+######LOGOUT METHODS######
+	get '/logout' do
+	    session[:user_id] = nil
+	    redirect '/login'
+    end
+######NEW USER######
+	get '/new_user' do
+		erb :new_user
+	end
+
+	post '/new_user' do
+		User.create(username: params[:username])
+		redirect to '/'
+	end
+######HELPER METHODS######
+	def current_user
+	    if logged_in?
+	    	User.find(session[:user_id])
+	    else
+	    	nil
+	    end
+	end
+
+    def logged_in?
+      session[:user_id]
+    end
 end
