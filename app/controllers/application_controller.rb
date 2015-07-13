c18991358318f0feb5cce61da014f5ebc54ca803
Charles Bailey <CharlesBailey@Charless-MacBook-Pro.local>
2015-07-13 13:11:17 -0400

    built app controller, added user_id to post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ require "sinatra/base"
 
 class ApplicationController < Sinatra::Base
 
-register Sinatra::Flash
+# register Sinatra::Flash
 
 	configure do
 		set :public_folder, 'public'
@@ -14,16 +14,54 @@ register Sinatra::Flash
 		set :sessions, true
 		set :session_secret, "/plz_dnt_hrt_mh"
 	end
-
+######ROOT PAGE METHODS######
 	get '/' do
 		@users = User.all
 		@posts = Post.all
 		erb :index
 	end
 
-	post '/' do
-		
+	post '/' do	
 		@posts = Post.all
 	end
+######LOGIN METHODS######
+	get '/login' do
+        erb :login
+  	end
 
+  	post '/login' do
+	    @user = User.find_by(username: params[:username])
+	    if @user
+	      session[:user_id] = @user.id
+	      redirect to '/'
+	    else
+	      redirect to '/login'
+	    end
+  	end
+######LOGOUT METHODS######
+	get '/logout' do
+	    session[:user_id] = nil
+	    redirect '/login'
+    end
+######NEW USER######
+	get '/new_user' do
+		erb :new_user
+	end
+
+	post '/new_user' do
+		User.create(username: params[:username])
+		redirect to '/'
+	end
+######HELPER METHODS######
+	def current_user
+	    if logged_in?
+	    	User.find(session[:user_id])
+	    else
+	    	nil
+	    end
+	end
+
+    def logged_in?
+      session[:user_id]
+    end
 end
